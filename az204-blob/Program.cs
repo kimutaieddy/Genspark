2614bded3bfed5b5ad9ac1d7256cf918e9ce0254
Kimutaieddy <eddykimutai@outlook.com>
2025-03-11 09:47:56 +0300

    Update storage connection string in Program.cs for Azure Blob Storage

--- az204-blob/Program.cs
+++ az204-blob/Program.cs
@@ -12,7 +12,7 @@ Console.ReadLine();
 static async Task ProcessAsync()
 {
     // Copy the connection string from the portal in the variable below.
-    string storageConnectionString = "CONNECTION STRING";
+    string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=angleblobstorage;AccountKey=26SUk4HIS4xyEHXSIwGgTsOJBUQt6sG6YJl5zWdGMb5nHqBiIbT5mOZmQSsYuNLt5Sk22iYf9dcH+AStG770YA==;EndpointSuffix=core.windows.net";
 
     // Create a client that can authenticate with a connection string
     BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
